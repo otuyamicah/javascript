b4007c54dff96d6c80e5b03b32a76101b31c4dd0
otuyamicah <otuyamicah@gmail.com>
2020-10-28 00:35:21 -1100

    javascript_basics

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+*thiw is example of a multiline comment
+*javascript basics
+*Example1 using the various data types
+*
+*/
+console.log('============ Intro & data types ===========')
+console.log(' ')
+
+
+console.log('hi javascript this is Micah calling from another file')
+console.log('Otuya')    //sample of logging to console
+console.log('Micah')    //this is an inline comment
+console.log('Maluabuchukwu')
+var name ='Micah'    //this is a string data type
+var age =30          //this is a number data type
+var isAwake = true   //this boolean data type
+var naWaoh           //this is undefined data type
+console.log(name)
+console.log(age)
+console.log(isAwake)
+console.log(naWaoh)
+var Null ='try'
+console.log(null)
+console.log('============ variable mutation and coercion ===========')
+console.log()
+var firstname = 'Micah'
+var age = 60
+console.log(firstname + ' is ' + age + ' years' ) //using concatination to join the different data types
+var job = 'engineer'
+var ismarried = 'true'
+console.log(job)
+console.log(ismarried)
+var name = 'Micah'
+var age = 30
+var job = 'Barber'
+var isMarried = 'true'
+console.log(name + ' is a ' + age + ' year old ' + job +  ', is he married? ' +  isMarried ) //type coercion
+age = 'twenty eight'
+job = 'driver'
+alert(name + ' is a ' + age + ' year old ' + job )
+
+var lastName = prompt('what is your last name?')
+console.log(firstname + ' ' + lastName)
